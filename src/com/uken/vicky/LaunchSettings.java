d72111006a3b2fd316276a70da06f049047a774c
Diogo Terror <me@diogoterror.com>
2014-01-24 19:00:55 -0500

    fixes and loop!

--- src/com/uken/vicky/LaunchSettings.java
+++ src/com/uken/vicky/LaunchSettings.java
@@ -1,5 +1,19 @@
 package com.uken.vicky;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.util.Log;
 
 import com.android.uiautomator.core.UiObject;
@@ -9,7 +23,8 @@ import com.android.uiautomator.core.UiSelector;
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
 public class LaunchSettings extends UiAutomatorTestCase {
-	
+	HttpClient httpClient = new DefaultHttpClient();
+
 	public void testDemo() throws UiObjectNotFoundException, InterruptedException {
 	  Log.i("vicky", "here");
       // Simulate a short press on the HOME button.
@@ -65,11 +80,15 @@ public class LaunchSettings extends UiAutomatorTestCase {
 		conversation.clickAndWaitForNewWindow();
 
 		// Reads Text
-		UiObject text = new UiObject(find("ListView-3.LinearLayout-2.TextView-0").instance(1));
-		String userInput = text.getText();
-
-		// Talk to Server, get response
-		String response = getResponseFor(userInput.toString());
+		UiObject text = new UiObject(find("ListView-2.LinearLayout-2.TextView-0").instance(0));
+		String response;
+		if (text.exists()) {
+			String userInput = text.getText();
+			// Get response from the server
+			response = getResponseFor(userInput.toString());
+		} else {
+			response = "What?";
+		}
 
 		// Write response back
 		UiObject textBox = new UiObject(find("EditText-0"));
@@ -88,9 +107,38 @@ public class LaunchSettings extends UiAutomatorTestCase {
 	}
 
 	
-	private String getResponseFor(String userInput) {
-		return "Did you just say '" + userInput + "'?";
+	private String getResponseFor(String userInput){
+//		return "Did you just say '" + userInput + "'?";
+		System.out.println("gettting response");
+		String encodedInput;
+		try {
+			encodedInput = URLEncoder.encode(userInput, "utf-8");
+		} catch (UnsupportedEncodingException e1) {
+			e1.printStackTrace();
+			return "I beg your pardon?";
+		}
+//		String endpoint = "http://api.program-o.com/v2.3.1/chatbot/?bot_id=6&convo_id=4815162342&say=" + encodedInput;
+		String endpoint = "http://192.168.32.51:8080/hubot/tell/?message=" + encodedInput;
+		HttpGet get = new HttpGet(endpoint);
+		try {
+			HttpResponse response = httpClient.execute(get);
+			String res = EntityUtils.toString(response.getEntity());
+			JSONObject json = new JSONObject(res);
+			System.out.println("resposne" + res);
+//			return json.getString("botsay");
+			return json.getString("message");
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+			return "I'm sorry, I'm suffering a horrible case of ClientProtocolException";
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "I'm sorry, I'm suffering a horrible case of IOException";
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return "I'm sorry, I'm suffering a horrible case of JSONException";
+		}
 	}
+
 
 	private UiSelector find(String selectString) throws UiObjectNotFoundException {
 		UiSelector currentSel = new UiSelector().index(0);
